d077f8180a1c1871d544e80c1207c01a47226393
ctargino <caio.targino@sambatech.com.br>
2021-05-16 18:43:32 -0300

    feat: add user favorite product repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,4 +3,12 @@ import { container } from 'tsyringe';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 
+import UserFavoriteProductRepository from '@modules/users/infra/typeorm/repositories/UserFavoriteProductRepository';
+import IUserFavoriteProductRepository from '@modules/users/repositories/IUserFavoriteProductRepository';
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<IUserFavoriteProductRepository>(
+  'UserFavoriteProductRepository',
+  UserFavoriteProductRepository,
+);
