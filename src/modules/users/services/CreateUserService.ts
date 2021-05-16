f2778de89a8c4071441e2e0b0e057f4c9c2af16f
ctargino <caio.targino@sambatech.com.br>
2021-05-15 23:34:28 -0300

    refactor: add token in create user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,9 +1,11 @@
 import { inject, injectable } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
 
 import { ICreateUserDTO } from '@modules/users/dtos/IUserDTO';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import AppError from '@shared/errors/AppError';
+import authConfig from '@shared/config/auth';
 
 @injectable()
 export default class CreateUserService {
@@ -15,6 +17,16 @@ export default class CreateUserService {
   public async execute(data: ICreateUserDTO): Promise<User> {
     try {
       const user = await this.userRepository.create(data);
+
+      const { secret, expiresIn } = authConfig.jwt;
+      const token = sign({}, secret, {
+        subject: String(user.user_id),
+        expiresIn,
+      });
+
+      user.token = token;
+
+      await this.userRepository.update(user.user_id, { token });
 
       return user;
     } catch (err) {
