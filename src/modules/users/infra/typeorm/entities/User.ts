bbbfa0239f715c21506c703d062c311af6e7c6df
ctargino <caio.targino@sambatech.com.br>
2021-05-15 22:22:21 -0300

    feat: add user table and migration

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,43 @@
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import bcrypt from 'bcryptjs';
+
+@Entity('user')
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
+  user_id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  token: string;
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+  created_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp with time zone' })
+  deleted_at: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    if (this.password) {
+      this.password = bcrypt.hashSync(this.password, 8);
+    }
+  }
+}
